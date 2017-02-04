3b61dc4fda2b27c74340354c915ba1085ac66a36
Hulva Luva.H <luva.l.hua@newegg.com>
2017-02-04 14:31:51 +0800

    add [Learning Hadoop 2] code example

---
+++ ForLearn/src/main/java/hulva/luva/learn/yaml/model/Level.java
@@ -0,0 +1,12 @@
+package hulva.luva.learn.yaml.model;
+
+/**
+ * @ClassName
+ * @Description
+ * @author Hulva Luva.H
+ * @date 2017年1月26日
+ *
+ */
+public enum Level {
+  ONE, TWO, THREE, FOUR, FIVE
+}
