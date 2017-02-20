efc6f3635781e048aa56f99affeabee2d4952806
Hulva Luva.H <lvsweet217@163.com>
2017-02-20 19:58:13 +0800

    add sub-project bigdata

---
+++ ForLearn/bigdata/src/main/java/hulva/luva/learn/bigdata/hbase/CreateHbaseTable.java
@@ -0,0 +1,94 @@
+/**
+ * 
+ */
+package hulva.luva.learn.bigdata.hbase;
+
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HConstants;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.io.compress.Compression.Algorithm;
+
+/**
+ * @author hulva
+ *
+ */
+public class CreateHbaseTable {
+	private static final String TABLE_NAME = "t_luva";
+	private static final String CF_DEFAULT = "cf_luva";
+
+	public static void createOrOverwrite(Admin admin, HTableDescriptor table) throws IOException {
+		if (admin.tableExists(table.getTableName())) {
+			admin.disableTable(table.getTableName());
+			admin.deleteTable(table.getTableName());
+		}
+		admin.createTable(table);
+	}
+
+	public static void createSchemaTables(Configuration config) throws IOException {
+		try (Connection connection = ConnectionFactory.createConnection(config); Admin admin = connection.getAdmin()) {
+
+			HTableDescriptor table = new HTableDescriptor(TableName.valueOf(TABLE_NAME));
+			table.addFamily(new HColumnDescriptor(CF_DEFAULT).setCompressionType(Algorithm.NONE));
+
+			System.out.print("Creating table. ");
+			createOrOverwrite(admin, table);
+			System.out.println(" Done.");
+		}
+	}
+
+	public static void modifySchema(Configuration config) throws IOException {
+		try (Connection connection = ConnectionFactory.createConnection(config); Admin admin = connection.getAdmin()) {
+
+			TableName tableName = TableName.valueOf(TABLE_NAME);
+			if (!admin.tableExists(tableName)) {
+				System.out.println("Table does not exist.");
+				System.exit(-1);
+			}
+
+			HTableDescriptor table = admin.getTableDescriptor(tableName);
+
+			// Update existing table
+			HColumnDescriptor newColumn = new HColumnDescriptor("NEWCF");
+			newColumn.setCompactionCompressionType(Algorithm.GZ);
+			newColumn.setMaxVersions(HConstants.ALL_VERSIONS);
+			admin.addColumn(tableName, newColumn);
+
+			// Update existing column family
+			HColumnDescriptor existingColumn = new HColumnDescriptor(CF_DEFAULT);
+			existingColumn.setCompactionCompressionType(Algorithm.GZ);
+			existingColumn.setMaxVersions(HConstants.ALL_VERSIONS);
+			table.modifyFamily(existingColumn);
+			admin.modifyTable(tableName, table);
+
+			// Disable an existing table
+			admin.disableTable(tableName);
+
+			// Delete an existing column family
+			admin.deleteColumn(tableName, CF_DEFAULT.getBytes("UTF-8"));
+
+			// Delete a table (Need to be disabled first)
+			admin.deleteTable(tableName);
+		}
+	}
+
+	public static void main(String... args) throws IOException {
+		Configuration config = HBaseConfiguration.create();
+
+		// Add any necessary configuration files (hbase-site.xml, core-site.xml)
+//		config.addResource("hbase-site.xml");
+//		config.addResource("core-site.xml");
+		config.set("hbase.zookeeper.quorum", "c7001.hulva.luva.h,c7003.hulva.luva.h,c7002.hulva.luva.h");
+		config.set("hbase.zookeeper.property.clientPort", "2181");
+		createSchemaTables(config);
+		modifySchema(config);
+	}
+}
