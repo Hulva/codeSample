53933f66082d1d613dda9c9b706f72f974b3f0cf
Hulva Luva.H <lvsweet217@163.com>
2017-11-28 20:43:32 +0800

    add configuration folder

--- java-mongodb/src/main/java/com/baeldung/MongoExample.java
+++ java-mongodb/src/main/java/com/baeldung/MongoExample.java
@@ -1,53 +1,64 @@
 package com.baeldung;
 
+import org.bson.Document;
+
 import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBCursor;
 import com.mongodb.MongoClient;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
 
 public class MongoExample {
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        MongoClient mongoClient = new MongoClient("localhost", 27017);
+		try (MongoClient mongoClient = new MongoClient("192.168.1.248", 27017);) {
 
-        DB database = mongoClient.getDB("myMongoDb");
+			MongoDatabase database = mongoClient.getDatabase("myMongoDb");
 
-        // print existing databases
-        mongoClient.getDatabaseNames().forEach(System.out::println);
+			// print existing databases
+			MongoCursor<String> dbNames = mongoClient.listDatabaseNames().iterator();
+			while (dbNames.hasNext()) {
+				System.out.println(dbNames.next());
+			}
 
-        database.createCollection("customers", null);
+//			database.createCollection("customers", null);
 
-        // print all collections in customers database
-        database.getCollectionNames().forEach(System.out::println);
+			// print all collections in customers database
+			MongoCursor<String> collections = database.listCollectionNames().iterator();
+			while (collections.hasNext()) {
+				System.out.println(collections.next());
+			}
 
-        // create data
-        DBCollection collection = database.getCollection("customers");
-        BasicDBObject document = new BasicDBObject();
-        document.put("name", "Shubham");
-        document.put("company", "Baeldung");
-        collection.insert(document);
+			// create data
+			MongoCollection<Document> collection = database.getCollection("customers");
+			BasicDBObject document = new BasicDBObject();
+			document.put("name", "Shubham");
+			document.put("company", "Baeldung");
+			collection.createIndex(document);
 
-        // update data
-        BasicDBObject query = new BasicDBObject();
-        query.put("name", "Shubham");
-        BasicDBObject newDocument = new BasicDBObject();
-        newDocument.put("name", "John");
-        BasicDBObject updateObject = new BasicDBObject();
-        updateObject.put("$set", newDocument);
-        collection.update(query, updateObject);
+			// update data
+			BasicDBObject query = new BasicDBObject();
+			query.put("name", "Shubham");
+			BasicDBObject newDocument = new BasicDBObject();
+			newDocument.put("name", "John");
+			BasicDBObject updateObject = new BasicDBObject();
+			updateObject.put("$set", newDocument);
+			collection.updateOne(query, updateObject);
 
-        // read data
-        BasicDBObject searchQuery = new BasicDBObject();
-        searchQuery.put("name", "John");
-        DBCursor cursor = collection.find(searchQuery);
-        while (cursor.hasNext()) {
-            System.out.println(cursor.next());
-        }
+			// read data
+			BasicDBObject searchQuery = new BasicDBObject();
+			searchQuery.put("name", "John");
+			FindIterable<Document> documents = collection.find(searchQuery);
+			MongoCursor<Document> cursor = documents.iterator();
+			while (cursor.hasNext()) {
+				System.out.println(cursor.next());
+			}
 
-        // delete data
-        BasicDBObject deleteQuery = new BasicDBObject();
-        deleteQuery.put("name", "John");
-        collection.remove(deleteQuery);
-    }
+			// delete data
+			BasicDBObject deleteQuery = new BasicDBObject();
+			deleteQuery.put("name", "John");
+			collection.deleteOne(deleteQuery);
+		}
+	}
 }
